3d17da425ded9d4b3b2f52eeb2ffd3d65843c8c1
sakkara04 <sakkara@ucsc.edu>
2024-11-21 14:03:14 -0800

    Added datetime to html

--- compass/src/app/app.component.ts
+++ compass/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatetimeComponent } from './datetime/datetime.component';
 import { WeeklyGoalsComponent } from './weekly-goals/weekly-goals.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
 
@@ -7,7 +8,7 @@ import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-mod
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent],
+  imports: [DatetimeComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
