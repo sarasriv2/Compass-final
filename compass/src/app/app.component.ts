b1f405a77dfa213c63c1174c73311cc807b95fdc
eves79 <evstanle@ucsc.edu>
2024-12-01 20:16:47 -0800

    made hashtag notes files and added html

--- compass/src/app/app.component.ts
+++ compass/src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { DatetimeComponent } from './datetime/datetime.component';
 import { WeeklyGoalsComponent } from './weekly-goals/weekly-goals.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
+import { GreetingComponent } from "./greeting/greeting.component";
+import { HashtagNotesComponent } from "./hashtag-notes/hashtag-notes.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatetimeComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent],
+  imports: [DatetimeComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent, GreetingComponent, HashtagNotesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
