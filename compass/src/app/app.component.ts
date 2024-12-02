162f3e86b0053dfcdcfbb7e889a640280521cd7b
sakkara04 <sakkara@ucsc.edu>
2024-12-02 02:37:14 -0800

    Did part of long term goals comp

--- compass/src/app/app.component.ts
+++ compass/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatetimeComponent } from './datetime/datetime.component';
 import { GreetingComponent } from './greeting/greeting.component';
+import { LongTermGoalsComponent } from './long-term-goals/long-term-goals.component';
 import { NavbarComponent } from './navbar/navbar.component'
 import { WeeklyGoalsComponent } from './weekly-goals/weekly-goals.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
@@ -11,7 +12,7 @@ import { HashtagNotesComponent } from './hashtag-notes/hashtag-notes.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatetimeComponent, GreetingComponent, NavbarComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent, HashtagNotesComponent],
+  imports: [DatetimeComponent, GreetingComponent, LongTermGoalsComponent, NavbarComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent, HashtagNotesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
