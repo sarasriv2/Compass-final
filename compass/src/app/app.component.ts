fa66b166ed03f0a669810104f4df53a5cd8c0932
sakkara04 <sakkara@ucsc.edu>
2024-12-02 01:10:47 -0800

    Fixed most of the layout

--- compass/src/app/app.component.ts
+++ compass/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DatetimeComponent } from './datetime/datetime.component';
+import { GreetingComponent } from './greeting/greeting.component';
 import { WeeklyGoalsComponent } from './weekly-goals/weekly-goals.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
 import { HashtagNotesComponent } from './hashtag-notes/hashtag-notes.component';
@@ -8,7 +9,7 @@ import { HashtagNotesComponent } from './hashtag-notes/hashtag-notes.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatetimeComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent, HashtagNotesComponent],
+  imports: [DatetimeComponent, GreetingComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent, HashtagNotesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
