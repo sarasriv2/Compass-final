4c826ab5bc3fbbb093ba3705331174d585fb0278
eves79 <evstanle@ucsc.edu>
2024-12-01 22:26:26 -0800

    made minor changes

--- compass/src/app/app.component.ts
+++ compass/src/app/app.component.ts
@@ -3,14 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { DatetimeComponent } from './datetime/datetime.component';
 import { WeeklyGoalsComponent } from './weekly-goals/weekly-goals.component';
 import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
-import { GreetingComponent } from "./greeting/greeting.component";
-import { HashtagNotesComponent } from "./hashtag-notes/hashtag-notes.component";
+import { HashtagNotesComponent } from './hashtag-notes/hashtag-notes.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatetimeComponent, RouterOutlet, WeeklyGoalsComponent, WeeklyGoalsModalComponent, GreetingComponent, HashtagNotesComponent],
+  imports: [DatetimeComponent, RouterOutlet, WeeklyGoalsComponent, HashtagNotesComponent, WeeklyGoalsModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -18,3 +17,5 @@ import { HashtagNotesComponent } from "./hashtag-notes/hashtag-notes.component";
 export class AppComponent {
   title = 'sakkara-midterm';
 }
+
+
