8b35f55b7fccf4b64ff8bdf7adeac0017dfc6b54
sara <ssriva16@ucsc.edu>
2024-12-03 17:47:07 -0800

    fixed datetime

--- compass/src/app/datetime/datetime.component.ts
+++ compass/src/app/datetime/datetime.component.ts
@@ -2,23 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-datetime',
-  standalone: true,
-  imports: [],
+  standalone: true,  
+  imports: [], 
   templateUrl: './datetime.component.html',
-  styleUrl: './datetime.component.scss'
+  styleUrls: ['./datetime.component.scss']
 })
-
-export class DatetimeComponent implements OnInit {
-  seconds: string = '00';
-
-  ngOnInit(): void {
-    this.updateSeconds();
-    setInterval(() => this.updateSeconds(), 1000);
-  }
-
-  private updateSeconds(): void {
-    const now = new Date();
-    const sec = now.getSeconds();
-    this.seconds = sec < 10 ? `0${sec}` : sec.toString();
-  }
+export class DatetimeComponent{
 }
